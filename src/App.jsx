97d4dc61b324a799dd7cb1c3f728225829a58e1d
Richard Essuman <ressuman001@gmail.com>
2024-07-24 22:02:28 +0000

    chore: Update dependencies and add NextUI components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,57 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Navigate,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import { RootLayout } from "./layouts/Root/RootLayout";
+
+import { Home } from "./pages/Home/Home";
+import LogIn from "./pages/Login/LogIn";
+import { SignUp } from "./pages/SignUp/SignUp";
+import Transactions from "./pages/Transactions/Transactions";
+import DashBoard from "./pages/DashBoard/DashBoard";
+
+import Content from "./components/Content/Content";
+
 export default function App() {
-  return <div></div>;
+  // Create routes based on user authentication state
+  const routes = createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route
+        index
+        element={userDetails ? <Navigate to="/dashboard" /> : <Home />}
+      />
+      <Route
+        path="signup"
+        element={userDetails ? <Navigate to="/dashboard" /> : <SignUp />}
+      />
+      <Route
+        path="login"
+        element={userDetails ? <Navigate to="/dashboard" /> : <LogIn />}
+      />
+      <Route
+        path="dashboard"
+        element={userDetails ? <DashBoard /> : <Navigate to="/login" />}
+      >
+        <Route index element={<Content />} />
+        <Route path="transactions" element={<Transactions />} />
+      </Route>
+    </Route>
+  );
+
+  const router = createBrowserRouter(routes);
+
+  const styles = {
+    loadingContainer: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+    },
+  };
+
+  return <RouterProvider router={router} />;
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,12 @@
+import { Link } from "@nextui-org/react";
+
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="min-h-screen flex items-center justify-center gap-x-4">
+        <Link href="/signup">Sign Up</Link>
+        <Link href="/login">Log In</Link>
+      </div>
+    </div>
+  );
 };
